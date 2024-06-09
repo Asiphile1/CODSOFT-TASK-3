f4380a416fdf31da7058e052f0701d311446e9fd
asiphile <asiphilemthethwa@gmail.comgit config --global user.name asiphilegit config --global user.email Asiphilemthethwa@gmail.comgit config --global user.email asiphilemthethwa@gmail.com>
2024-06-09 04:43:13 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+})
+
+
+//the JavaScript for this code needed a sound understanding of JavaScript I think I got all the correct principles so far
